3df55bd55e0011ce82c6a68143e405ac9cd94995
Roger Mannby <roger@rm-eltech.com>
2018-08-19 23:47:27 +0200

    Refactor the pubnub gateway to faster update after new connect

--- pubnub-gateway.js
+++ pubnub-gateway.js
@@ -1,78 +1,86 @@
+$(document).ready(function () {
+  console.log('Document ready')
+
+});
+
+function updateDOM(res) {
+  //  console.log(res)
+  if (res["Glassroom"] === "-99.9") {
+    glassRoomTemp.innerHTML = ("--.-" + "°C");
+  } else if (res["Glassroom"]) {
+    glassRoomTemp.innerHTML = (res["Glassroom"] + "°C");
+  }
+
+  if (res["indoor"] === "-99.9") {
+    livingRoomTemp.innerHTML = ("--.-" + "°C");
+  } else if (res["indoor"]) {
+    livingRoomTemp.innerHTML = (res["indoor"] + "°C");
+  }
+
+  if (res["Outdoor north"] === "-99.9") {
+    outsideNorthTemp.innerHTML = ("--.-" + "°C");
+  } else if (res["Outdoor north"]) {
+    outsideNorthTemp.innerHTML = res["Outdoor north"] + "°C";
+  }
+
+  if (res["Outdoor south"] === "-99.9") {
+    outsideSouthTemp.innerHTML = ("--.-" + "°C");
+  } else if (res["Outdoor south"]) {
+    outsideSouthTemp.innerHTML = res["Outdoor south"] + "°C";
+  }
+
+  if (res["Garage"] === "-99.9") {
+    garageTemp.innerHTML = ("--.-" + "°C");
+  } else if (res["Garage"]) {
+    garageTemp.innerHTML = (res["Garage"] + "°C");
+  }
+
+  if (res["Pool"] === "-99.9") {
+    poolTemp.innerHTML = ("--.-" + "°C");
+  } else if (res["Pool"]) {
+    poolTemp.innerHTML = (res["Pool"] + "°C");
+  }
+
+  if (res["Poolheat"] === "-99.9") {
+    poolHeatTemp.innerHTML = ("--.-" + "°C");
+  } else if (res["Poolheat"]) {
+    poolHeatTemp.innerHTML = (res["Poolheat"] + "°C");
+  }
+  //minMaxIn.innerHTML = (res["Poolheat"] + "°C");
+  // Show/hide mouse direction
+  // console.log(res["Mouse trapped"]);
+  // var mouse = document.getElementById("icon-mouse-trap");
+  // if (res["Mouse trapped"] === "Trip") {
+  //   mouse.style.display = "inline";
+  // } else {
+  //   mouse.style.display = "none";
+  // }
+  
+  getTime();
+}
+
 (function () {
-  var pubnub = PUBNUB.init({
+  let pubnub = PUBNUB.init({
     publish_key: 'pub-c-6a121d53-b962-4a48-b425-10281417b24d',
     subscribe_key: 'sub-c-9e12300c-4af3-11e7-bf50-02ee2ddab7fe',
     uuid: 'mannbyUUID',
     ssl: true
   });
-  var box = PUBNUB.$('box'),
-    input = PUBNUB.$('input'),
-    channel = 'RpiGate';
+
+  let channel = 'RpiGate'
 
   pubnub.subscribe({
     channel: channel,
-    callback: function (text) {
-      if (text["Glassroom"] === "-99.9") {
-        glassRoomTemp.innerHTML = ("--.-" + "°C");
-      } else {
-        glassRoomTemp.innerHTML = (text["Glassroom"] + "°C");
-      }
-
-      if (text["indoor"] === "-99.9") {
-        livingRoomTemp.innerHTML = ("--.-" + "°C");
-      } else {
-        livingRoomTemp.innerHTML = (text["indoor"] + "°C");
-      }
-
-      if (text["Outdoor north"] === "-99.9") {
-        outsideNorthTemp.innerHTML = ("--.-" + "°C");
-      } else {
-        outsideNorthTemp.innerHTML = text["Outdoor north"] + "°C";
-      }
-
-      if (text["Outdoor south"] === "-99.9") {
-        outsideSouthTemp.innerHTML = ("--.-" + "°C");
-      } else {
-        outsideSouthTemp.innerHTML = text["Outdoor south"] + "°C";
-      }
-
-      if (text["Garage"] === "-99.9") {
-        garageTemp.innerHTML = ("--.-" + "°C");
-      } else {
-        garageTemp.innerHTML = (text["Garage"] + "°C");
-      }
-
-      if (text["Pool"] === "-99.9") {
-        poolTemp.innerHTML = ("--.-" + "°C");
-      } else {
-        poolTemp.innerHTML = (text["Pool"] + "°C");
-      }
-
-      if (text["Poolheat"] === "-99.9") {
-        poolHeatTemp.innerHTML = ("--.-" + "°C");
-      } else {
-        poolHeatTemp.innerHTML = (text["Poolheat"] + "°C");
-      }
-      //minMaxIn.innerHTML = (text["Poolheat"] + "°C");
-      // Show/hide mouse direction
-      // console.log(text["Mouse trapped"]);
-      // var mouse = document.getElementById("icon-mouse-trap");
-      // if (text["Mouse trapped"] === "Trip") {
-      //   mouse.style.display = "inline";
-      // } else {
-      //   mouse.style.display = "none";
-      // }
-      getTime();
+    callback: (res) => updateDOM(res),
+    connect: () => {
+      console.log('PubNub channel connected')
+      pubnub.publish({
+        channel: channel,
+        message: 'connected'
+      })
     }
-  });
-
-  //PUBNUB.bind('keyup', input, function (e) {
-  //    (e.keyCode || e.charCode) === 13 && pubnub.publish({
-  //        channel: channel, message: input.value, x: (input.value = '')
-  //    })
-  //})
-})();
-
+  })
+})()
 
 function getTime() {
 
